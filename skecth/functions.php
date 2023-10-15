ac599bfffbae6dae30b8288fb5c248585bd0e64f
mississppi <msspstudio@gmail.com>
2023-10-15 21:43:07 +0900

    一旦保存

---
+++ skecth/functions.php
@@ -0,0 +1,7 @@
+<?php
+
+function mytheme_setup(){
+    
+}
+
+add_action('after_setup_theme', 'setup');
--- skecth/index.php
+++ skecth/index.php
@@ -1,11 +1,49 @@
 <!DOCTYPE html>
-<html <?php ?>>
+<html <?php language_attributes(); ?>>
 <head>
-    <meta charset="UTF-8">
+    <meta charset="<?php bloginfo('charset')?>">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Document</title>
+
+    <link rel="stylesheet" href="https://fonts.googleapis.com/css?family=Noto+Sans+JP:400,700,900&display=swap&subset=japanese" rel="stylesheets">
+    <link rel="stylesheet" href="style.css">
 </head>
 <body>
+    <header class="header">
+        <div class="container">
+            <div class="site">
+                <img src="" alt="">
+                <div>
+                    <p>テストカンパニー</p>
+                    <p>確かなサポート</p>
+                </div>
+            </div>
+
+            <button type="button" class="nav-button">
+                <span class="sr-only">MENU</span>
+            </button>
+        </div>
+    </header>
+
+    <section class="hero">
+        <div class="catch">
+            <h1>
+                <img src="" alt="">
+            </h1>
+            <p>お仕事を支えます</p>
+        </div>
+    </section>
+
+    <section class="message">
+        <div class="container">
+            <h2>ビジネスアシスタントとして<br>サポートを続けて30年</h2>
+            <p>プロのビジネスアシスタントとして独立して以来、幅広いクライアントからの依頼に応じ、業務の多様性や事業の効率化などをサポートしてまいりました。当社のスタッフはさまざまな分野に精通しており、必要に応じて様々な業務に対応できます。雑務に費やす時間を最小限に抑え、クライアント様が重要なプロジェクトに専念できるようお手伝いしています。</p>
+        </div>
+    </section>
     
+    <section class="service"></section>
+    <section class="contact"></section>
+
+    <footer></footer>
 </body>
 </html>
