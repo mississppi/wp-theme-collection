f13c335e2fcf0fea85a4bde7142b9d460fb20538
mississppi <msspstudio@gmail.com>
2023-10-16 16:59:41 +0900

    headerとfooterを作成

---
+++ skecth/footer.php
@@ -0,0 +1,31 @@
+<footer class="footer">
+        <div class="container">
+            <div class="info">
+                <div class="site">
+                    <img src="" alt="">
+                    <div>
+                        <p class="corp">テストカンパニー</p>
+                    </div>
+                </div>
+                <p class="add">
+                    〒001-0001<br>
+                    北海道中央区南99条東西ビル10F<br>
+                    011-1111-2222
+                </p>
+            </div>
+
+            <ul class="sns">
+                <li><a href="#"><img src="" alt="Twitter"></a></li>
+                <li><a href="#"><img src="" alt="Facebook"></a></li>
+                <li><a href="#"><img src="" alt="Instagram"></a></li>
+            </ul>
+        </div>
+    </footer>
+
+    <script>
+        function navFunc(){
+            document.querySelector('html').classList.toggle('open');
+        }
+    </script>
+</body>
+</html>
--- skecth/index.php
+++ skecth/index.php
@@ -1,38 +1,4 @@
-<?php ?>
-<!DOCTYPE html>
-<html <?php language_attributes(); ?>>
-<head>
-    <meta charset="<?php bloginfo('charset')?>">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Document</title>
-
-    <link rel="stylesheet" href="https://fonts.googleapis.com/css?family=Noto+Sans+JP:400,700,900&display=swap&subset=japanese" rel="stylesheets">
-    <?php wp_head(); ?>
-</head>
-<body>
-    <header class="header">
-        <div class="container">
-            <div class="site">
-                <img src="<?php echo get_template_directory_uri(); ?>/assets/images/w.svg?<?php echo date("YmdHis");?>" alt="" />
-                <div>
-                    <p class="corp">テストカンパニー</p>
-                    <p class="desc">確かなサポート</p>
-                </div>
-            </div>
-
-            <nav class="nav">
-                <ul>
-                    <li><a href="#">会社情報</a></li>
-                    <li><a href="#">サービス</a></li>
-                    <li><a href="#" class="btn">お問い合わせ</a></li>
-                </ul>
-            </nav>
-
-            <button type="button" class="nav-button" onClick="navFunc()">
-                <span class="sr-only">MENU</span>
-            </button>
-        </div>
-    </header>
+<?php get_header(); ?>
 
     <section class="hero">
         <div class="catch">
@@ -101,34 +67,4 @@
         </div>
     </section>
 
-    <footer class="footer">
-        <div class="container">
-            <div class="info">
-                <div class="site">
-                    <img src="" alt="">
-                    <div>
-                        <p class="corp">テストカンパニー</p>
-                    </div>
-                </div>
-                <p class="add">
-                    〒001-0001<br>
-                    北海道中央区南99条東西ビル10F<br>
-                    011-1111-2222
-                </p>
-            </div>
-
-            <ul class="sns">
-                <li><a href="#"><img src="" alt="Twitter"></a></li>
-                <li><a href="#"><img src="" alt="Facebook"></a></li>
-                <li><a href="#"><img src="" alt="Instagram"></a></li>
-            </ul>
-        </div>
-    </footer>
-
-    <script>
-        function navFunc(){
-            document.querySelector('html').classList.toggle('open');
-        }
-    </script>
-</body>
-</html>
+<?php get_footer(); ?>
